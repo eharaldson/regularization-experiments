1e85868f1cfbefd8f59843c87fb3e7619392431f
Erik Haraldson <erikharaldson@MacBook-Pro.lan>
2022-12-26 21:29:38 +0100

    Plots an underfitted model

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from sklearn import model_selection, linear_model
+
 def expand_squared_values(x):
     return np.hstack((x, np.square(x)))
 
@@ -18,10 +20,23 @@ def create_polynomial_labels(x):
     weights = np.array([1, 3, -6])
     bias = 2
     y = x @ weights + bias
-    return y
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.3)
+
+    return x_train, x_test, y_train, y_test
 
 x = np.random.rand(20,1)
-y = create_polynomial_labels(x)
+x_train, x_test, y_train, y_test = create_polynomial_labels(x)
 
-plt.scatter(x, y)
+x_train_1 = x_train[:,0]
+x_train_1 = x_train_1.reshape(-1,1)
+
+print(x_train_1.shape)
+
+model = linear_model.LinearRegression()
+model.fit(x_train_1, y_train)
+
+y_prediction = model.predict(x_train_1)
+
+plt.scatter(x_train_1, y_train)
+plt.plot(x_train_1, y_prediction, c='orange')
 plt.show()
