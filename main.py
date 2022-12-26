f850e526d60187d45a75d5fcc946f5cfb51d50dd
Erik Haraldson <erikharaldson@MacBook-Pro.lan>
2022-12-26 21:07:59 +0100

    Generalises the function to expand the matrix up to a certain power

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ import numpy as np
 def expand_squared_values(x):
     return np.hstack((x, np.square(x)))
 
+def expand_power_of_values(x, power):
+    return_x = x
+    for i in range(2,power+1):
+        x_to_add = x ** i
+        x_to_add = x_to_add.reshape(-1,1)
+        return_x = np.concatenate((return_x, x_to_add), axis=1)
+    return return_x
+
 matrix = np.random.rand(20,1)
 
+matrix = expand_power_of_values(matrix, 4)
 print(matrix)
-print(expand_squared_values(matrix))
